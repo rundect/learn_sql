f52a5ccc251d104004080471a4d69bf7a6f06d18
develop <dofastsite@gmail.com>
2025-01-09 16:26:22 +0300

    Solve problem 7

---
+++ sql-ex/7.sql
@@ -0,0 +1,14 @@
+select PC.model, PC.price
+from Product join
+PC on Product.model = PC.model
+where Product.maker = 'B'
+union
+select Laptop.model, Laptop.price
+from Product join
+Laptop on Product.model = Laptop.model
+where Product.maker = 'B'
+union
+select Printer.model, Printer.price
+from Product join
+Printer on Product.model = Printer.model
+where Product.maker = 'B';
